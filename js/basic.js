5c33624527580b95e599eb83caa35b7aca19dc90
TuomasLattila <tuomas.lattila@outlook.com>
2023-06-10 14:59:43 +0300

    Added table for users, fieldset with textinputs for creating new users and a table empty button.

--- js/basic.js
+++ js/basic.js
@@ -1,17 +1,65 @@
-console.log("hello world");
-console.log("lool");
+const submitButton = document.getElementById("submit-data")
+const emptyButton = document.getElementById("empty-table")
 
-function sum(a,b)   {
-    console.log(a+b)
+let tableBody = document.getElementById("tableBody")
+let newUsername = document.getElementById("input-username")
+let newEmail = document.getElementById("input-email")
+let newAddress = document.getElementById("input-address")
+let adminCB = document.getElementById("input-admin")
+
+let rowCount = 0
+
+let users = []
+
+submitButton.addEventListener("click", () => {
+
+    users.push({username: newUsername.value, email: newEmail.value, address: newAddress.value, admin: adminCB.checked})
+    updateTable(users)
+
+    // newUsername.value = ""
+    // newEmail.value = ""
+    // newAddress.value = ""
+    // adminCB.checked = false
+})
+
+emptyButton.addEventListener("click", () => {
+    emptyTable()
+    while(users.length > 0) {
+        users.pop()
+    }
+})
+
+function updateTable(users) {
+    emptyTable()
+    users.forEach(user => {
+        let tableRow = document.createElement("tr")
+        let tableColumn1 = document.createElement("td")
+        tableColumn1.innerText = user.username
+        let tableColumn2 = document.createElement("td")
+        tableColumn2.innerText = user.email
+        let tableColumn3 = document.createElement("td")
+        tableColumn3.innerText = user.address
+        let tableColumn4 = document.createElement("td")
+        if (user.admin == true)    {
+            tableColumn4.innerText = "X"
+        } else  {
+            tableColumn4.innerText = "-"
+        }
+
+        tableRow.appendChild(tableColumn1)
+        tableRow.appendChild(tableColumn2)
+        tableRow.appendChild(tableColumn3)
+        tableRow.appendChild(tableColumn4)
+        tableBody.appendChild(tableRow)
+
+        rowCount++
+    });
 }
 
-let sum2 = function(a, b)   {
-    return a+b
+function emptyTable()   {
+    while(tableBody.hasChildNodes())  {
+        tableBody.removeChild(tableBody.firstChild)
+    }
+    
 }
-
-setTimeout(function()   {console.log("2 seconds has passed")}, 2000)
-
-let button = document.getElementById("btn")
-
-button.addEventListener("click", () => sum(6,9))
 
